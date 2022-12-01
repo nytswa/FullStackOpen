37693044a01527946912ee9d4002fb796154a806
nytswa <dark.nitsua@gmail.com>
2022-12-01 13:49:55 -0300

    4.4: helper functions and unit tests, step2

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -1,22 +1,104 @@
-const listHelper = require('../utils/list_helper')
+const dummy = require('../utils/list_helper').dummy
+const totalLikes = require('../utils/list_helper').totalLikes
 
-test('dummy returns one', () => {
-  const blogs = []
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+const listWithThreeEqualBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const listWithFiveBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+
+describe('dummy', () => {
+  test('returns one', () => {
+    const blogs = []
+  
+    const result = dummy(blogs)
+    expect(result).toBe(1)
+  })
+  
+  test('1 element number returns one', () => expect(dummy([0])).toBe(1))
+  
+  test('1 element string returns one', () => expect(dummy(['a'])).toBe(1))
 })
 
-test('dummy of 1 element number returns one', () => {
-  const blogs = [1]
-
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
-
-test('dummy of 1 element string returns one', () => {
-  const blogs = ['a']
-
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+describe('total likes ', () => {
+  test('of empty list is zero', () => expect(totalLikes([])).toBe(0))
+  test('when list has only blog equals the likes of that', () => expect(totalLikes(listWithOneBlog)).toBe(5))
+  test('of a bigger list is calculated right', () => expect(totalLikes(listWithThreeEqualBlogs)).toBe(15))
+  test('of 5 blogs', () => expect(totalLikes(listWithFiveBlogs)).toBe(25))
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -4,6 +4,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
