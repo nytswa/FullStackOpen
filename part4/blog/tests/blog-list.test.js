00417f131a9a3e4533239b5749035edbd8575ac7
nytswa <dark.nitsua@gmail.com>
2022-12-03 11:50:51 -0300

    b.0 more integrated tests
    check 1 object + POST test

--- part4/blog/tests/blog-list.test.js
+++ part4/blog/tests/blog-list.test.js
@@ -2,8 +2,34 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../index').server_app
 const server = require('../index').server
+const Blog = require('../models/Blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    "author": "Austin Feltan",
+    "title": "My Personal Life",
+    "url": "111",
+    "likes": 1
+  },
+  {
+    "author": "Austin Feltan",
+    "title": "Studies",
+    "url": "222",
+    "likes": 2
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blog1 = new Blog(initialBlogs[0])
+  await blog1.save()
+
+  const blog2 = new Blog(initialBlogs[1])
+  await blog2.save()
+})
 
 test('returned values are objects/json', async () => {
   await api
@@ -14,9 +40,45 @@ test('returned values are objects/json', async () => {
 
 test('json returned', async () => {
   const response = await api.get('/api/blogs')
-  // expect(response.body).toHaveLenght(2)
   expect(typeof response.body).toBe('object')
   expect(typeof response.body).not.toBe('array')
+})
+
+test('returned length', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(initialBlogs.length)
+})
+
+test('check first blog: author, title, url', async () => {
+  const response = await api.get('/api/blogs')
+  
+  const authors = response.body.map(blog => blog.author)
+  const titles = response.body.map(blog => blog.title)
+  expect(authors).toContain(initialBlogs[0].author)
+  expect(titles).toContain(initialBlogs[0].title)
+  
+  expect(response.body[0].url).toBe(initialBlogs[0].url)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    "author": "None",
+    "title": "Nothing",
+    "url": "333",
+    "likes": 3
+  }
+
+  await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)  // Created
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const authors = response.body.map(blog => blog.author)
+  const titles = response.body.map(blog => blog.title)
+  expect(authors).toContain(newBlog.author)
+  expect(titles).toContain(newBlog.title)
 })
 
 afterAll(() => {
