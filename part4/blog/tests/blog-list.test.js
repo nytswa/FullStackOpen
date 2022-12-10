4ebdfe1afe3f0befed4fbd0cbb47129ca1e8c1a6
nytswa <dark.nitsua@gmail.com>
2022-12-10 10:47:37 -0300

    4.12b refactoring tests
    Describe: Grouped Testing

--- part4/blog/tests/blog-list.test.js
+++ part4/blog/tests/blog-list.test.js
@@ -14,130 +14,134 @@ beforeEach(async () => {
   await blog2.save()
 })
 
-test.skip('returned values are objects/json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-    .expect(typeof response.body).toBe('object')
-    .expect(typeof response.body).not.toBe('array')
-}, 12000)
-
-test.skip('returned length', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body.length).toBe(initialBlogs.length)
-  expect(response.body).toHaveLength(initialBlogs.length)  // same as above
-}, 12000)
-
-test('ID unique identifier property named correctly', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body[0].id).toBeDefined()
-}, 12000)
-
-test('check first blog: author, title, url', async () => {
-  const response = await api.get('/api/blogs')
+describe('Initial saved data/blogs', () => {
+  test.skip('returned values are objects/json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect(typeof response.body).toBe('object')
+      .expect(typeof response.body).not.toBe('array')
+  }, 12000)
   
-  const authors = response.body.map(blog => blog.author)
-  const titles = response.body.map(blog => blog.title)
-  expect(authors).toContain(initialBlogs[0].author)
-  expect(titles).toContain(initialBlogs[0].title)
+  test.skip('returned length', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(initialBlogs.length)
+    expect(response.body).toHaveLength(initialBlogs.length)  // same as above
+  }, 12000)
   
-  expect(response.body[0].url).toBe(initialBlogs[0].url)
-}, 12000)
-
-test('a valid blog can be added', async () => {
-  const newVBlog = {
-    "author": "None",
-    "title": "Nothing",
-    "url": "333",
-    "likes": 3
-  }
-
-  await api.post('/api/blogs')
-    .send(newVBlog)
-    .expect(201)  // Created
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-
-  const authors = response.body.map(blog => blog.author)
-  const titles = response.body.map(blog => blog.title)
-  expect(authors).toContain(newVBlog.author)
-  expect(titles).toContain(newVBlog.title)
-}, 22000)
-
-test('an invalid blog with no author is not added', async () => {
-  const newInBlog = {
-    "title": "Nothing",
-    "url": "333",
-    "likes": 3
-  }
-
-  await api.post('/api/blogs')
-    .send(newInBlog)
-    .expect(400)  // Created
-
-  const { authors, titles } = await getAllBlogs()
-  expect(authors).not.toContain('None')
-  expect(titles).not.toContain(newInBlog.title)
-}, 22000)
-
-test('an invalid blog with no title is not added', async () => {
-  const newInBlog = {
-    "author": "None",
-    "url": "333",
-    "likes": 3
-  }
-
-  await api.post('/api/blogs')
-    .send(newInBlog)
-    .expect(400)  // Created
-
-  const response = await api.get('/api/blogs')
+  test('ID: unique identifier property named correctly', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  }, 12000)
   
-  const authors = response.body.map(blog => blog.author)
-  const titles = response.body.map(blog => blog.title)
-  expect(authors).not.toContain(newInBlog.author)
-  expect(titles).not.toContain('Nothing')
-}, 22000)
+  test('check first blog: author, title, url', async () => {
+    const response = await api.get('/api/blogs')
+    
+    const authors = response.body.map(blog => blog.author)
+    const titles = response.body.map(blog => blog.title)
+    expect(authors).toContain(initialBlogs[0].author)
+    expect(titles).toContain(initialBlogs[0].title)
+    
+    expect(response.body[0].url).toBe(initialBlogs[0].url)
+  }, 12000)
+})
 
-test('an invalid blog with no url is not added', async () => {
-  const newInBlog = {
-    "title": "Nothing",
-    "author": "None",
-    "likes": 4
-  }
-
-  const responsePost = await api.post('/api/blogs')
-    .send(newInBlog)
-    .expect(400)  // Created
+describe('POST Created', () => {
+  test('a valid blog can be added', async () => {
+    const newVBlog = {
+      "author": "None",
+      "title": "Nothing",
+      "url": "333",
+      "likes": 3
+    }
   
-  expect(responsePost.body).toEqual({ error: 'URL or Title/Author is missing' })
-
-  const response = await api.get('/api/blogs')
+    await api.post('/api/blogs')
+      .send(newVBlog)
+      .expect(201)  // Created
+      .expect('Content-Type', /application\/json/)
   
-  const authors = response.body.map(blog => blog.author)
-  const titles = response.body.map(blog => blog.title)
-  expect(authors).not.toContain(newInBlog.author)
-  expect(titles).not.toContain(newInBlog.title)
-}, 22000)
+    const response = await api.get('/api/blogs')
+  
+    const authors = response.body.map(blog => blog.author)
+    const titles = response.body.map(blog => blog.title)
+    expect(authors).toContain(newVBlog.author)
+    expect(titles).toContain(newVBlog.title)
+  }, 22000)
+  
+  test('likes missing from request will still be 0', async () => {
+    const newInBlog = {
+      "author": "None",
+      "title": "Nothing",
+      "url": "333"
+    }
+  
+    const createdBlog = await api.post('/api/blogs')
+      .send(newInBlog)
+      .expect(201)  // Created
+  
+    expect(createdBlog.body.url).toBe('333')
+    expect(createdBlog.body.likes).toBe(0)
+  }, 22000)
+})
 
-test('likes missing from request will still be 0', async () => {
-  const newInBlog = {
-    "author": "None",
-    "title": "Nothing",
-    "url": "333"
-  }
-
-  const createdBlog = await api.post('/api/blogs')
-    .send(newInBlog)
-    .expect(201)  // Created
-
-  expect(createdBlog.body.url).toBe('333')
-  expect(createdBlog.body.likes).toBe(0)
-}, 22000)
-
-
+describe('invalid blogs', () => {
+  test('with no author is not added', async () => {
+    const newInBlog = {
+      "title": "Nothing",
+      "url": "333",
+      "likes": 3
+    }
+  
+    await api.post('/api/blogs')
+      .send(newInBlog)
+      .expect(400)  // Created
+  
+    const { authors, titles } = await getAllBlogs()
+    expect(authors).not.toContain('None')
+    expect(titles).not.toContain(newInBlog.title)
+  }, 22000)
+  
+  test('with no title is not added', async () => {
+    const newInBlog = {
+      "author": "None",
+      "url": "333",
+      "likes": 3
+    }
+  
+    await api.post('/api/blogs')
+      .send(newInBlog)
+      .expect(400)  // Created
+  
+    const response = await api.get('/api/blogs')
+    
+    const authors = response.body.map(blog => blog.author)
+    const titles = response.body.map(blog => blog.title)
+    expect(authors).not.toContain(newInBlog.author)
+    expect(titles).not.toContain('Nothing')
+  }, 22000)
+  
+  test('with no url is not added', async () => {
+    const newInBlog = {
+      "title": "Nothing",
+      "author": "None",
+      "likes": 4
+    }
+  
+    const responsePost = await api.post('/api/blogs')
+      .send(newInBlog)
+      .expect(400)  // Created
+    
+    expect(responsePost.body).toEqual({ error: 'URL or Title/Author is missing' })
+  
+    const response = await api.get('/api/blogs')
+    
+    const authors = response.body.map(blog => blog.author)
+    const titles = response.body.map(blog => blog.title)
+    expect(authors).not.toContain(newInBlog.author)
+    expect(titles).not.toContain(newInBlog.title)
+  }, 22000)
+})
 
 
 afterAll(() => {
